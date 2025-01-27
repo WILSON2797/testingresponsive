45d7031884e6f125f1ba5e8465301c60423b7acd
WILSON2797 <gurningwilson@gmail.com>
2025-01-28 00:41:34 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,48 +1,16 @@
-//menu
-var tombolMenu = $(".tombol-menu");
-var menu = $("nav .menu ul");
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('#hamburger-menu');
+    const navbarNav = document.querySelector('.navbar-nav');
 
-function klikMenu() {
-    tombolMenu.click(function () {
-        menu.toggle();
+    hamburger.addEventListener('click', function(e) {
+        navbarNav.classList.toggle('active');
+        e.preventDefault();
     });
-    menu.click(function () {
-        menu.toggle();
-    });
-}
 
-$(document).ready(function () {
-    var width = $(window).width();
-    if (width < 990) {
-        klikMenu();
-    }
-})
-
-//check lebar
-$(window).resize(function () {
-    var width = $(window).width();
-    if (width > 989) {
-        menu.css("display", "block");
-        //display:block
-    } else {
-        menu.css("display", "none");
-    }
-    klikMenu();
-});
-
-//efek scroll 
-$(document).ready(function () {
-    var scroll_pos = 0;
-    $(document).scroll(function () {
-        scroll_pos = $(this).scrollTop();
-        if (scroll_pos > 0) {
-            $("nav").addClass("putih");
-            $("nav img.hitam").show();
-            $("nav img.putih").hide();
-        } else {
-            $("nav").removeClass("putih");
-            $("nav img.hitam").hide();
-            $("nav img.putih").show();
+    // Klik di luar sidebar untuk menghilangkan nav
+    document.addEventListener('click', function(e) {
+        if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+            navbarNav.classList.remove('active');
         }
-    })
+    });
 });
